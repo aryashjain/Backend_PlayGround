a75fef4fb5f0c44cf7b030f8996acd638c2fb2f0
Aryash Jain <aryash.enu@gmail.com>
2024-01-22 23:19:41 +0530

    feat: added utilities -ApiResponse -ApiError -AsyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,8 @@
-const asyncHandler = () => {}
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
 
-export { asyncHandler}
+export { asyncHandler }
