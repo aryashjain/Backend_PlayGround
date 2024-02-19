6ef5f09919d0135b927c0769d09cd314af2a4a6b
Aryash Jain <aryash.enu@gmail.com>
2024-02-19 18:00:41 +0530

    fix:updated cloudenry

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,17 +10,23 @@ cloudinary.config({
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
+
+        if (!localFilePath) {
+         console.log("did not get local file path")
+            return null
+        }
+        console.log("LOCAL FILE PATH ==",localFilePath);
+            //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
         // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
+        console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
+        console.log("ERROR IN UPLOADING FILE==",error)
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
