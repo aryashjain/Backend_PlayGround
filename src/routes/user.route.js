b47e4b8f0ca00a2c8f143ad21e14fdb9e6624dfe
Aryash Jain <aryash.enu@gmail.com>
2024-02-20 18:18:20 +0530

    feat: -added login,register,logout,refreshAccessToken Routes -added auth middleware

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
 
@@ -18,6 +20,13 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,logoutUser)
+// verify jwt will happen at the login and so the cookie willbe set so we can acces it directly
+router.route("/refreshToken").post(refreshAccessToken)
 
 
 export default router
